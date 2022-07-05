3a468911e2de7b2996283653ded41de279c9eb28
Somashri Roy <>
2022-07-05 13:33:20 +0530

    Changes in run_test.sh file

--- run_test.sh
+++ run_test.sh
@@ -2,6 +2,7 @@ with_docker=$1
 if [ "$with_docker" = true ];then
     docker build -t cucumber-api-automation-framework .
     docker run --name apiautomation -i cucumber-api-automation-framework mvn test
+    echo "Running in docker container"
     docker rm -f apiautomation
 else
 	mvn test
